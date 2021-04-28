624fe71229dcdd883c42634e05913d0ad34a3d74
Rustam Valiullin <rvcoding@gmail.com>
2021-04-28 13:03:24 +0300

    added new directives column-body-template.directive, column-edit-template.directive, colu,column-foot-template.directive, column-head-template.directive

--- projects/ngx-qgrid/src/lib/column/column.module.ts
+++ projects/ngx-qgrid/src/lib/column/column.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { ColumnComponent } from './column.component';
-import { ColumnDirective } from './column.directive';
-
+import { ColumnBodyTemplateDirective } from './column-body-template.directive';
+import { ColumnEditTemplateDirective } from './column-edit-template.directive';
+import { ColumnFootTemplateDirective } from './column-foot-template.directive';
+import { ColumnHeadTemplateDirective } from './column-head-template.directive';
 @NgModule({
 	declarations: [
 		ColumnComponent,
-		ColumnDirective
+		ColumnBodyTemplateDirective,
+		ColumnEditTemplateDirective,
+		ColumnFootTemplateDirective,
+		ColumnHeadTemplateDirective
 	],
 	exports: [
 		ColumnComponent,
-		ColumnDirective
+		ColumnBodyTemplateDirective,
+		ColumnEditTemplateDirective,
+		ColumnFootTemplateDirective,
+		ColumnHeadTemplateDirective
 	]
 })
 export class ColumnModule {
